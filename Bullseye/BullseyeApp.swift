4f18dc37e097a4b0c0360e34c4b48e5da79816aa
Marty Heavey <martyheavey@gmail.com>
2021-03-01 22:07:54 -0500

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Marty Heavey on 3/1/21.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
